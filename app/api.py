221be5e7720fd5e88fdef2a526c3aa60a55c9fc7
Julian <julianqp5025@icloud.com>
2024-07-25 12:16:19 -0500

    Change the way to insert_one record with POST

--- app/api.py
+++ app/api.py
@@ -81,7 +81,29 @@ async def create_user(user: UserModel = Body(...), current_access: UserAccess = 
   if (convert_to_json(users_collection.find_one({"_id": user.id}))) is not None:
     raise HTTPException(status_code=409, detail=f"Duplicate record for id: {user.id}")
 
-  new_user = users_collection.insert_one(user.model_dump(by_alias=True))
+  new_user = users_collection.insert_one({
+      "_id": user.id,
+      "fec_alta": user.fec_alta,
+      "user_name": user.user_name,
+      "codigo_zip": user.codigo_zip,
+      "credit_card_num": b'user.credit_card_num',
+      "credit_card_ccv": b'user.credit_card_ccv',
+      "cuenta_numero": b'user.cuenta_numero',
+      "direccion": user.direccion,
+      "geo_latitud": user.geo_latitud,
+      "geo_longitud": user.geo_longitud,
+      "color_favorito": user.color_favorito,
+      "foto_dni": user.foto_dni,
+      "ip": user.ip,
+      "auto": user.auto,
+      "auto_modelo": user.auto_modelo,
+      "auto_tipo": user.auto_tipo,
+      "auto_color": user.auto_color,
+      "cantidad_compras_realizadas": user.cantidad_compras_realizadas,
+      "avatar": user.avatar,
+      "fec_birthday": user.fec_birthday
+    }
+  )
   create_user = users_collection.find_one({"_id": new_user.inserted_id})
   return create_user
 
